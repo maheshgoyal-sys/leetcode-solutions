201a5e5237b00364f2422ddce7862e4d317329be
Mahesh Goyal <maheshgoyal20032004@gmail.com>
2026-03-17 20:18:42 +0530

    Time: 2 ms (90.45%), Space: 44.3 MB (51.48%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,55 +1,52 @@
 class Solution {
+    class Pair{
+        int i;
+        int j;
+        Pair(int i,int j){
+            this.i=i;
+            this.j=j;
+        }
+    }
     public int orangesRotting(int[][] grid) {
-
         int m = grid.length;
         int n = grid[0].length;
-
-        Queue<int[]> q = new LinkedList<>();
         int freshcount = 0;
-        int time = 0;
-
+        Queue<Pair> q = new LinkedList<>();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
-                    q.add(new int[]{i,j});
+                    q.add(new Pair(i,j));
                 }
                 if(grid[i][j]==1){
                     freshcount++;
                 }
             }
         }
-
-        int dr[] = {-1,0,1,0};
-        int dc[] = {0,-1,0,1};
-
-        while(!q.isEmpty() && freshcount>0){
-
-            int size = q.size();
-            time++;
-
-            for(int i=0;i<size;i++){
-
-                int arr[] = q.poll();
-                int r = arr[0];
-                int c = arr[1];
-
-                for(int k=0;k<4;k++){
-
-                    int nr = r + dr[k];
-                    int nc = c + dc[k];
-
-                    if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1){
-
-                        grid[nr][nc] = 2;
-                        freshcount--;
-
-                        q.add(new int[]{nr,nc});
-                    }
+            int dr[] = {-1,0,1,0};
+            int dc[] = {0,-1,0,1};
+        if(freshcount==0)   return 0;
+        int time = 0;
+        while(!q.isEmpty()){
+           int size = q.size();
+             boolean changed = false;
+           for(int i=0;i<size;i++){
+            Pair pair = q.poll();
+            int r = pair.i;
+            int c = pair.j;
+            for(int k=0;k<4;k++){
+                int newrow = r+dr[k];
+                int newcol = c+dc[k];
+                if(newrow>=0 && newcol>=0 && newrow<m && newcol<n && grid[newrow][newcol]==1){
+                    grid[newrow][newcol]=2;
+                    q.add(new Pair(newrow,newcol));
+                    freshcount--;
+                    changed=true;
                 }
             }
-        }
+           }
+            if(changed) time++;
 
-        if(freshcount==0) return time;
-        return -1;
+        }
+        return freshcount==0 ? time : -1;
     }
 }
