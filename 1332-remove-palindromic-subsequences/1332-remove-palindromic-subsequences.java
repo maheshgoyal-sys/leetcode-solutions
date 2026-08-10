475a1850e4f60606bd5ec5f9e94511a690a0d3ce
Mahesh Goyal <maheshgoyal20032004@gmail.com>
2026-08-10 09:57:36 +0530

    Time: 0 ms (100%), Space: 42.5 MB (81.5%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removePalindromeSub(String s) {
+        int l=0,r=s.length()-1;
+        while(l<r){
+            if(s.charAt(l)!=s.charAt(r)){
+                return 2;
+            }
+            l++;
+            r--;
+        }
+        return 1;
+    }
+}
